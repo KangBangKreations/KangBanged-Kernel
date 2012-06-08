8854971cdacb85e41c77b47c2d20844498837bab
Synergy Dev <synergycomputing@gmail.com>
2012-06-08 15:35:29 -0700

    msm restart: update to full CAF compliance

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -60,6 +60,7 @@
 #define SCM_IO_DISABLE_PMIC_ARBITER	1
 
 static int restart_mode;
+void *restart_reason;
 
 struct htc_reboot_params {
 	unsigned reboot_reason;
@@ -185,31 +186,6 @@ static inline void set_restart_msg(const char *msg)
 	strncpy(reboot_params->msg, msg, sizeof(reboot_params->msg)-1);
 }
 
-static bool console_flushed;
-
-static void msm_pm_flush_console(void)
-{
-	if (console_flushed)
-		return;
-	console_flushed = true;
-
-	printk("\n");
-	printk(KERN_EMERG "Restarting %s\n", linux_banner);
-	if (!console_trylock()) {
-		console_unlock();
-		return;
-	}
-
-	mdelay(50);
-
-	local_irq_disable();
-	if (console_trylock())
-		printk(KERN_EMERG "restart: Console was locked! Busting\n");
-	else
-		printk(KERN_EMERG "restart: Console was locked!\n");
-	console_unlock();
-}
-
 /* This function expose others to restart message for entering ramdump mode. */
 void set_ramdump_reason(const char *msg)
 {
@@ -248,7 +224,7 @@ void notify_modem_efs_sync(unsigned timeout)
 	writel(32768 * 13, msm_tmr0_base + WDT0_BITE_TIME);
 	writel(3, msm_tmr0_base + WDT0_EN);
 	dsb();
-	set_WDT_EN_footprint(3);
+//	set_WDT_EN_footprint(3); comment out for CAF compatibility
 	wait_rmt_final_call_back(timeout);
 	/* Rest watchdog timer to make sure we have enough time to power off */
 	writel(1, msm_tmr0_base + WDT0_RST);
@@ -337,9 +313,6 @@ inline void soc_restart(char mode, const char *cmd)
 
 void arch_reset(char mode, const char *cmd)
 {
-	/* arch_reset should only enter once*/
-	if (atomic_add_return(1, &restart_counter) != 1)
-		return;
 
 #ifdef CONFIG_MSM_DLOAD_MODE
 
@@ -359,130 +332,22 @@ void arch_reset(char mode, const char *cmd)
 #endif
 
 	printk(KERN_NOTICE "Going down for restart now\n");
-	printk(KERN_NOTICE "%s: mode %d\n", __func__, mode);
-	if (cmd) {
-		printk(KERN_NOTICE "%s: restart command `%s'.\n", __func__, cmd);
-		/* XXX: modem will set msg itself.
-			Dying msg should be passed to this function directly. */
-		if (mode != RESTART_MODE_MODEM_CRASH)
-			set_restart_msg(cmd);
-	} else
-		printk(KERN_NOTICE "%s: no command restart.\n", __func__);
 
-#ifdef CONFIG_ARCH_MSM8960
-	if (pm8xxx_reset_pwr_off(1)) {
-		in_panic = 1;
-	}
-#else
 	pm8xxx_reset_pwr_off(1);
-#endif
 
-	if (in_panic) {
-		/* Reason & message are set in panic notifier, panic_prep_restart.
-		   Kernel panic is a top priority.
-		   Keep this condition to avoid the reason and message being modified. */
-	} else if (!cmd) {
-		set_restart_reason(RESTART_REASON_REBOOT);
-	} else if (!strncmp(cmd, "bootloader", 10)) {
-		set_restart_reason(RESTART_REASON_BOOTLOADER);
-	} else if (!strncmp(cmd, "recovery", 8)) {
-		set_restart_reason(RESTART_REASON_RECOVERY);
-	} else if (!strcmp(cmd, "eraseflash")) {
-		set_restart_reason(RESTART_REASON_ERASE_FLASH);
-	} else if (!strncmp(cmd, "oem-", 4)) {
-		unsigned long code;
-		code = simple_strtoul(cmd + 4, 0, 16) & 0xff;
-
-		/* oem-97, 98, 99 are RIL fatal */
-		if ((code == 0x97) || (code == 0x98))
-			code = 0x99;
-
-		set_restart_reason(RESTART_REASON_OEM_BASE | code);
-	} else if (!strcmp(cmd, "force-hard") ||
-			(RESTART_MODE_LEGECY < mode && mode < RESTART_MODE_MAX)
-		) {
-		/* The only situation modem user triggers reset is NV restore after erasing EFS. */
-		if (mode == RESTART_MODE_MODEM_USER_INVOKED)
-			set_restart_reason(RESTART_REASON_REBOOT);
-		else
-			set_restart_reason(RESTART_REASON_RAMDUMP);
-	} else {
-		/* unknown command */
-		set_restart_reason(RESTART_REASON_REBOOT);
-	}
-
-#if defined(CONFIG_MSM_RMT_STORAGE_CLIENT)
-	/* if modem crashed, do not sync efs. */
-	if (mode != RESTART_MODE_MODEM_CRASH &&
-		mode != RESTART_MODE_MODEM_UNWEDGE_TIMEOUT &&
-		mode != RESTART_MODE_MODEM_WATCHDOG_BITE &&
-		mode != RESTART_MODE_MODEM_ERROR_FATAL &&
-		mode != RESTART_MODE_APP_WATCHDOG_BARK
-		) {
-		/* final efs_sync */
-		printk(KERN_INFO "from %s\r\n", __func__);
-
-		/* efs sync timeout is 10 seconds.
-		   set watchdog to 12 seconds. */
-		writel(1, msm_tmr0_base + WDT0_RST);
-		writel(0, msm_tmr0_base + WDT0_EN);
-		writel(32768 * 12, msm_tmr0_base + WDT0_BARK_TIME);
-		writel(32768 * 13, msm_tmr0_base + WDT0_BITE_TIME);
-		writel(3, msm_tmr0_base + WDT0_EN);
-		dsb();
-		set_WDT_EN_footprint(3);
-
-		/* radio team would like to be notified when MDM_DOG_BITE and MDM_FATAL */
-		if (in_panic || (mode == RESTART_MODE_MDM_DOG_BITE) || (mode == RESTART_MODE_MDM_FATAL)) {
-			rmt_storage_set_msm_client_status(0);
-			smsm_change_state(SMSM_APPS_STATE, SMSM_APPS_REBOOT, SMSM_APPS_REBOOT);
+	if (cmd != NULL) {
+		if (!strncmp(cmd, "bootloader", 10)) {
+			__raw_writel(0x77665500, restart_reason);
+		} else if (!strncmp(cmd, "recovery", 8)) {
+			__raw_writel(0x77665502, restart_reason);
+		} else if (!strncmp(cmd, "oem-", 4)) {
+			unsigned long code;
+			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
+			__raw_writel(0x6f656d00 | code, restart_reason);
 		} else {
-			wait_rmt_final_call_back(10);
+			__raw_writel(0x77665501, restart_reason);
 		}
-		printk(KERN_INFO "back %s\r\n", __func__);
 	}
-#endif
-
-	switch (get_restart_reason()) {
-	case RESTART_REASON_RIL_FATAL:
-	case RESTART_REASON_RAMDUMP:
-		send_q6_nmi(); /* NMI trigger to get valid Q6 ramdump */
-		if (!in_panic && mode != RESTART_MODE_APP_WATCHDOG_BARK) {
-			/* Suspend wdog until all stacks are printed */
-			msm_watchdog_suspend(NULL);
-			dump_stack();
-			show_state_filter(TASK_UNINTERRUPTIBLE);
-			print_workqueue();
-			msm_watchdog_resume(NULL);
-		}
-		break;
-	}
-
-	/*	for kernel panic & ril fatal & MDM_DOG_BITE & MDM_FATAL,
-		kenrel needs waiting modem flushing caches at most 10 seconds. 	*/
-	if (in_panic || get_restart_reason() == RESTART_REASON_RIL_FATAL || (mode == RESTART_MODE_MDM_DOG_BITE) || (mode == RESTART_MODE_MDM_FATAL)) {
-		int timeout = 10;
-		printk(KERN_INFO "%s: wait for modem flushing caches.\n", __func__);
-		while (timeout > 0 && !modem_cache_flush_done) {
-			/* Kick watchdog.
-			   Do not assume efs sync will be executed.
-			   Assume watchdog timeout is default 4 seconds. */
-			writel(1, msm_tmr0_base + WDT0_RST);
-
-			mdelay(1000);
-			timeout--;
-		}
-		if (timeout <= 0)
-			printk(KERN_NOTICE "%s: modem flushes cache timeout.\n", __func__);
-	}
-
-#if defined(CONFIG_ARCH_MSM8X60_LTE)
-	/* Added by HTC for forcing mdm9K to do the cache flush */
-	if (mode == RESTART_MODE_MODEM_CRASH)
-		charm_panic_wait_mdm_shutdown();
-#endif	/* CONFIG_ARCH_MSM8X60_LTE */
-
-	msm_pm_flush_console();
 
 	__raw_writel(0, msm_tmr0_base + WDT0_EN);
 	if (!(machine_is_msm8x60_fusion() || machine_is_msm8x60_fusn_ffa())) {
@@ -501,11 +366,9 @@ void arch_reset(char mode, const char *cmd)
 	printk(KERN_ERR "Restarting has failed\n");
 }
 
-
 static int __init msm_restart_init(void)
 {
 	int rc;
-	arm_pm_restart = arch_reset;
 
 #ifdef CONFIG_MSM_DLOAD_MODE
 	atomic_notifier_chain_register(&panic_notifier_list, &panic_blk);
@@ -515,11 +378,8 @@ static int __init msm_restart_init(void)
 	set_dload_mode(1);
 #endif
 	msm_tmr0_base = msm_timer_get_timer0_base();
-	reboot_params = (void *)MSM_REBOOT_REASON_BASE;
-	memset(reboot_params, 0x0, sizeof(struct htc_reboot_params));
-	set_restart_reason(RESTART_REASON_RAMDUMP);
-	set_restart_msg("Unknown");
-	reboot_params->radio_flag = get_radio_flag();
+	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
+	pm_power_off = msm_power_off;
 
 	if (pmic_reset_irq != 0) {
 		rc = request_any_context_irq(pmic_reset_irq,
@@ -531,7 +391,6 @@ static int __init msm_restart_init(void)
 		pr_warn("no pmic restart interrupt specified\n");
 	}
 
-	pm_power_off = msm_power_off;
 	return 0;
 }
 
