31c023ff8c7efa07c949efc59717ee08f15cf7fb
Synergy Dev <synergycomputing@gmail.com>
2012-06-08 12:23:53 -0700

    add timer stats lte code

--- kernel/time/timer_stats.c
+++ kernel/time/timer_stats.c
@@ -392,6 +392,33 @@ static ssize_t tstats_write(struct file *file, const char __user *buf,
 	return count;
 }
 
+#ifdef CONFIG_ARCH_MSM8X60_LTE
+void htc_timer_stats_OnOff(char OnOff)
+{
+	mutex_lock(&show_mutex);
+	switch (OnOff) {
+	case '0':
+		if (timer_stats_active) {
+			timer_stats_active = 0;
+			time_stop = ktime_get();
+			sync_access();
+		}
+		break;
+	case '1':
+		if (!timer_stats_active) {
+			reset_entries();
+			time_start = ktime_get();
+			smp_mb();
+			timer_stats_active = 1;
+		}
+		break;
+	default:
+		break;
+	}
+	mutex_unlock(&show_mutex);
+}
+#endif
+
 static int tstats_open(struct inode *inode, struct file *filp)
 {
 	return single_open(filp, tstats_show, NULL);
