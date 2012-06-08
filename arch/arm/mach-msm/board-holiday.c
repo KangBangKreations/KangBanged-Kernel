88e43e7bc6cee99daccbb5ac7039713260bcfa8f
Synergy Dev <synergycomputing@gmail.com>
2012-06-08 16:01:32 -0700

    fix reg voltages

--- arch/arm/mach-msm/board-holiday.c
+++ arch/arm/mach-msm/board-holiday.c
@@ -506,8 +506,8 @@ static struct regulator_init_data saw_s0_init_data = {
 		.constraints = {
 			.name = "8901_s0",
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 700000,
-			.max_uV = 1450000,
+			.min_uV = 750000,
+			.max_uV = 1550000,
 		},
 		.consumer_supplies = vreg_consumers_8901_S0,
 		.num_consumer_supplies = ARRAY_SIZE(vreg_consumers_8901_S0),
@@ -517,8 +517,8 @@ static struct regulator_init_data saw_s1_init_data = {
 		.constraints = {
 			.name = "8901_s1",
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 700000,
-			.max_uV = 1450000,
+			.min_uV = 750000,
+			.max_uV = 1550000,
 		},
 		.consumer_supplies = vreg_consumers_8901_S1,
 		.num_consumer_supplies = ARRAY_SIZE(vreg_consumers_8901_S1),
