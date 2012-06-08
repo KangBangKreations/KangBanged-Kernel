0c4fad8560cc792d07747ecd6660b9c6d9dbf1e3
Synergy Dev <synergycomputing@gmail.com>
2012-06-07 22:55:16 -0700

    adapt mmc drivers to new CAF code

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1947,14 +1947,10 @@ static void mmc_blk_remove_req(struct mmc_blk_data *md)
 {
 	if (md) {
 		if (md->disk->flags & GENHD_FL_UP) {
-			/* Resume queue before enter gendisk_async.
-			 * Flush thread may be blocked by I/O and can not be stopped when queue thread is still suspended.
-			 */
-			mmc_queue_resume(&md->queue);
 			device_remove_file(disk_to_dev(md->disk), &md->force_ro);
 
 			/* Stop new requests from getting into the queue */
-			gendisk_async(md->disk);
+			del_gendisk(md->disk);
 		}
 
 		/* Then flush out any already in there */
