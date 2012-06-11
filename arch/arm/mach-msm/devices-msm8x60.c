db0e46b060a6febe2232c8bed97d92a562a16ffa
Synergy Dev <synergycomputing@gmail.com>
2012-06-10 19:07:42 -0700

    part2 of BUSOC Config

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -867,7 +867,11 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
+#ifdef CONFIG_MSM_KGSL_BUSOC
 		.ib = KGSL_CONVERT_TO_MBPS(1300),
+#else
+		.ib = KGSL_CONVERT_TO_MBPS(990),
+#endif
 	},
 };
 
